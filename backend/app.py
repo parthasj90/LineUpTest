aa4e3caf3063b0429795230ebc68182eba89a20f
PARTHA JENA <parthasj90@gmail.com>
2020-10-05 14:24:43 +0100

    backend working

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,24 @@
-import datetime
-import os
+import requests
  
-from flask import Flask, Response, request
+from flask import Flask, Response, jsonify, request
+from marshmallow import Schema, fields
 
 app = Flask(__name__)
 
-@app.route("/api")
-def index():
-    return "Hello from api"
+class UserDataSchema(Schema):
+    avatar = fields.Str()
+    email = fields.Email()
+    first_name = fields.Str(data_key="firstName")
+    id = fields.Integer()
+    last_name = fields.Str(data_key="lastName")
+
+UserSchema = UserDataSchema()
+
+@app.route("/api/<int:id>", methods=['GET'])
+def index(id):
+    r = requests.get("https://reqres.in/api/users/"+str(id))
+    data = UserSchema.dump(r.json()["data"])
+    return data
     
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
