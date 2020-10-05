7cb5eef7a1c80b16b4b6155a3447725d443e1bb7
PARTHA JENA <parthasj90@gmail.com>
2020-10-05 20:27:26 +0100

    dockerized both backend and frontend

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 import requests
- 
+import os
 from flask import Flask, Response, jsonify, request
 from marshmallow import Schema, fields
 from flask_cors import CORS
@@ -8,6 +8,7 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+
 class UserDataSchema(Schema):
     avatar = fields.Str()
     email = fields.Email()
@@ -15,13 +16,16 @@ class UserDataSchema(Schema):
     id = fields.Integer()
     last_name = fields.Str(data_key="lastName")
 
+
 UserSchema = UserDataSchema()
+
 
 @app.route("/api/<int:id>", methods=['GET'])
 def index(id):
     r = requests.get("https://reqres.in/api/users/"+str(id))
     data = UserSchema.dump(r.json()["data"])
     return data
-    
+
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=os.getenv('PORT'))
--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -4,11 +4,11 @@ export const FETCH_USER = "FETCH_USER";
 
 export function fetchUser(id) {
   console.log(id);
-  const url = `http://127.0.0.1:5000/api/${id}`;
+  const url = `http://localhost:5000/api/${id}`;
   const request = axios.get(url);
 
   return {
     type: FETCH_USER,
-    payload: request
+    payload: request,
   };
 }
