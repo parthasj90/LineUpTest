cf9bce462f84a0c7f750c51bcebf23854dd339ff
PARTHA JENA <parthasj90@gmail.com>
2020-10-05 18:15:46 +0100

    frontend cards working

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 export const FETCH_USER = "FETCH_USER";
 
-export function fetchUser() {
-  const url = `http://127.0.0.1:5000/api/1`;
+export function fetchUser(id) {
+  console.log(id);
+  const url = `http://127.0.0.1:5000/api/${id}`;
   const request = axios.get(url);
 
   return {
--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -2,26 +2,48 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { fetchUser } from "../../actions/index";
+import styled from "styled-components";
 
+const Card = styled.div`
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  text-align: center;
+  padding: 20px;
+`;
+const Container = styled.div`
+  padding: 2px 16px;
+`;
 class Profile extends Component {
   constructor(props) {
     super(props);
-    console.log(this.props.weather);
   }
 
   componentDidMount() {
-    this.props.fetchUser();
-
+    this.props.fetchUser(this.props.match.params.id);
   }
 
   render() {
+    let user = this.props.user.length > 0 ? this.props.user[0] : undefined;
     return (
       <div>
-        hello
- {this.props.user.length >0 ? this.props.user[0].avatar:""}
+        {user ? (
+          <Card>
+            <img src={user.avatar}></img>
+            <br />
+            <Container>
+              {`User Id : ${user.id}`}
+              <br />
+              {`Name: ${user.firstName} ${user.lastName}`}
+              <br />
+              {`Email : ${user.email}`}
+            </Container>
+
+            <br />
+          </Card>
+        ) : (
+          ""
+        )}
       </div>
-       
-    
     );
   }
 }
@@ -30,11 +52,11 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators({ fetchUser }, dispatch);
 }
 
-function mapStateToProps(state){
-  console.log(state)
-	return {
-  	user: state.user
-  }
+function mapStateToProps(state) {
+  console.log(state);
+  return {
+    user: state.user,
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Profile);
