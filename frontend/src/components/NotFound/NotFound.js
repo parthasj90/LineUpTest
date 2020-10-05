6d6ae66e6fdfcbdb58ad77acb7ad25f3195528d1
PARTHA JENA <parthasj90@gmail.com>
2020-10-05 23:28:43 +0100

    styling for notfound

--- frontend/src/components/NotFound/NotFound.js
+++ frontend/src/components/NotFound/NotFound.js
@@ -1,10 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 2px 16px;
+  text-align: center;
+`;
 
 function NotFound() {
   return (
-    <div>
-      Ooops..page is not available right now.Please Navigate to /users/id
-    </div>
+    <Container>
+      <h1>
+        Ooops..page is not available right now.Please Navigate to /users/id
+      </h1>
+    </Container>
   );
 }
 
